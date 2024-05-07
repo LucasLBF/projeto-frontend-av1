100a8b7d8205d3e06b07ec03839dba9dedadbcb7
Lucas Barros Fernandes <lbf2@CESAR-LDAP.INTRANET>
2024-05-06 23:04:13 -0300

    Add styling and back button

--- src/js/sign-up.js
+++ src/js/sign-up.js
@@ -1,10 +1,6 @@
-const createBackBtn = () => {
-    const form = document.getElementById("sign-up-form");
-    const backBtn = document.createElement("a");
+const linkBackBtn = () => {
+    const backBtn = document.getElementById("back");
     backBtn.href = document.referrer;
-    backBtn.className = "btn btn-danger";
-    backBtn.textContent = "Voltar"
-    form.appendChild(backBtn);
 }
 
-createBackBtn();
+linkBackBtn()
